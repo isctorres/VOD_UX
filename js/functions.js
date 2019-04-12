66d18192c83f745e58715abfd78fc5c03418dd95
isctorres <isctorres@MacBook-Pro-de-Office.local>
2019-04-12 11:34:37 -0500

    actualizacion

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+// slider
+var swiper = new Swiper('.s1', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper = new Swiper('.s2', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper = new Swiper('.s3', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  
